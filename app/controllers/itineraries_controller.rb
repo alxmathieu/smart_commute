8d0e324eea56439c70b13ce8df1135309387e831
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2019-02-10 23:18:37 +0100

    retrieving duration from google maps api, setting duration to object itinerary and displaying the text version on the show

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,15 +1,20 @@
+require 'open-uri'
 class ItinerariesController < ApplicationController
-
-
 
   def new
     @itinerary = Itinerary.new
   end
 
   def create
+    # destroy last itinerary for the user
     @last_itinerary = Itinerary.where(user_id: itinerary_params["user_id"]).last
     Itinerary.destroy(@last_itinerary.id) unless @last_itinerary.nil?
+    # create new itinerary
     @itinerary = Itinerary.new(itinerary_params)
+    # retrieve duration and set duration
+    url = build_link_for_google(@itinerary)
+    set_duration_for_itinerary(@itinerary, url)
+    # save itinerary
     if @itinerary.save
       redirect_to itinerary_path(@itinerary)
     end
@@ -18,17 +23,36 @@ class ItinerariesController < ApplicationController
 
   def show
     @itinerary = Itinerary.find(params[:id])
-    build_link_for_google(@itinerary)
+    # Construire le lien pour l'api google
+    url = build_link_for_google(@itinerary)
+    # Récupérer la data du json google et la duration
+    @duration = retrieve_duration_from_url(url)
   end
 
   private
 
   def build_link_for_google(itinerary)
     api_key = ENV['MAPS_API_KEY']
+    # Pour supprimer les espaces et rendre le point de départ/arrivée url-compliant
     origin = itinerary.start_point.gsub(' ','+')
     destination = itinerary.end_point.gsub(' ','+')
-    link = "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&mode=transit&key=#{api_key}"
-    raise
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&mode=transit&key=#{api_key}"
+  end
+
+  def retrieve_duration_from_url(url)
+    data = JSON.parse(open(url).read)
+    routes = data["routes"].first
+    legs = routes["legs"].first
+    duration = legs["duration"]["text"]
+  end
+
+  def set_duration_for_itinerary(itinerary, url)
+    data = JSON.parse(open(url).read)
+    routes = data["routes"].first
+    legs = routes["legs"].first
+    duration = legs["duration"]["value"]
+    itinerary.duration = duration
+    itinerary.save
   end
 
   def itinerary_params
