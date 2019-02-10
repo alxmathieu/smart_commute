96af13b5d659f8b2d02c8acb89e6437953f9271b
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2019-02-10 20:17:16 +0100

    building google link and adding show to itinerary

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -10,13 +10,23 @@ class ItinerariesController < ApplicationController
     @last_itinerary = Itinerary.where(user_id: itinerary_params["user_id"]).last
     Itinerary.destroy(@last_itinerary.id) unless @last_itinerary.nil?
     @itinerary = Itinerary.new(itinerary_params)
-    @itinerary.save
+    if @itinerary.save
+      redirect_to itinerary_path(@itinerary)
+    end
+
+  end
+
+  def show
+    @itinerary = Itinerary.find(params[:id])
   end
 
   private
 
-  def build_link_for_google
-
+  def build_link_for_google(itinerary)
+    api_key = ENV['MAPS_API_KEY']
+    origin = itinerary.start_point.gsub(' ','+')
+    destination = itinerary.end_point.gsub(' ','+')
+    link = "https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&mode=transit&key=#{api_key}"
   end
 
   def itinerary_params
