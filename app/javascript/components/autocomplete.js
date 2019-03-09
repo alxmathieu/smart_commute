ba40c6b32b5af9e7d845f059ee8e0420c9c3be06
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2019-03-09 22:38:40 +0100

    adding autocomplete on end point'

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,14 +1,26 @@
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var itineraryFormInput = document.getElementById('itinerary_start_point');
-    if (itineraryFormInput) {
-      var autocomplete = new google.maps.places.Autocomplete(itineraryFormInput, { types: [ 'geocode' ] });
-      google.maps.event.addDomListener(itineraryFormInput, 'keydown', function(e) {
+    var itineraryFormInputStart = document.getElementById('itinerary_start_point');
+    var itineraryFormInputEnd = document.getElementById('itinerary_end_point');
+
+    if (itineraryFormInputStart) {
+      var autocomplete = new google.maps.places.Autocomplete(itineraryFormInputStart, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(itineraryFormInputStart, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
         }
       });
     }
+
+    if (itineraryFormInputEnd) {
+      var autocomplete = new google.maps.places.Autocomplete(itineraryFormInputEnd, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(itineraryFormInputEnd, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+
   });
 }
 
