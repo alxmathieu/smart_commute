9f0b7cfc32878f4191e8fad85db4dfe1e4fb9c78
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2019-04-22 20:51:13 +0200

    adding profile page, and listing all seen suggestions

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   mount ForestLiana::Engine => '/forest'
+  get '/profile', to: 'pages#profile'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
