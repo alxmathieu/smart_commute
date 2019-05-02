973cb38d2f88d19d6aa6966313a6256d446ee0a3
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2019-05-03 00:06:50 +0200

    adding ajax calls to allow for watchlisting"

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -14,16 +14,44 @@ class SuggestionsController < ApplicationController
   end
 
   def add_to_watchlist
-    suggestion = Suggestion.find(params[:id])
-    suggestion.status = "watchlist"
-    suggestion.save!
+    @suggestion = Suggestion.find(params[:id])
+    @suggestion.watchlisted = true
+    #suggestion.update(watchlisted: true)
+    if @suggestion.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js  # <-- will render `app/views/reviews/add_to_watchlist.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'root' }
+        format.js  # <-- idem
+      end
+    end
+  end
+
+  def remove_from_watchlist
+    @suggestion = Suggestion.find(params[:id])
+    @suggestion.watchlisted = false
+    #suggestion.update(watchlisted: true)
+    if @suggestion.save
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js  # <-- will render `app/views/reviews/add_to_watchlist.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'root' }
+        format.js  # <-- idem
+      end
+    end
   end
 
 
 private
 
   def itinerary_params
-    params.require(:suggestion).permit(:itinerary_id, :inspiration_id, :user_id, :status)
+    params.require(:suggestion).permit(:itinerary_id, :inspiration_id, :user_id, :status, :watchlisted)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :suggestions
-  match 'suggestions/:id/add_to_watchlist' => 'suggestions#add_to_watchlist', as: :suggestion_watchlist, via: [:get, :post]
+  match 'suggestions/:id/add_to_watchlist' => 'suggestions#add_to_watchlist', as: :suggestion_add_watchlist, via: [:get, :post]
+  match 'suggestions/:id/remove_from_watchlist' => 'suggestions#remove_from_watchlist', as: :suggestion_remove_watchlist, via: [:get, :post]
   resources :itineraries # à retravailler pour n'avoir que les routes nécessaires
   devise_for :users
   root to: 'pages#home'
